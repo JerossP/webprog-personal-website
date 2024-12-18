f2a2ee266c1bc9ebd1f3d111ad79654dc626ca3c
JerossP <jrperez2@student.apc.edu.ph>
2024-12-18 18:07:31 +0800

    aaaaaaa

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,11 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+  document.getElementById("demo").innerHTML = "What pet do i have?";
+}
+
+function myFunction2() {
+  const element = document.getElementsByTagName("c");
+
+  document.getElementById("demo").innerHTML = 'A ' ' + element[0].innerHTML;
+
+
+}
