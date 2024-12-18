92e3af046b12ac56e948a33a4e76407b9068c34e
JerossP <jrperez2@student.apc.edu.ph>
2024-12-18 18:49:36 +0800

    clean

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,10 +1,12 @@
+
+
 function myFunction() {
   document.getElementById("demo").innerHTML = "What pet do I have?";
 }
 
 function myFunction2() {
   const element = document.getElementsByTagName("span"); 
-  
+
   document.getElementById("demo").innerHTML = 'A ' + element[0].innerHTML; 
 
 }
